24f7f6a1667ff2da48d634fa77c56433d3c200b6
federico-casu <federico.casu1101@gmail.com>
2024-01-19 15:40:34 +0100

    feat: added array of objects and printed their informations

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/* ----------------------------------------------------------------
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0: (OK)
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+Organizzare i singoli membri in card/schede
+
+
+Dati da usare:
+
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+---------------------------------------------------------------- */
+
+// creo l'array di oggetti
+const team = [
+    {
+        'nome': 'Wayne Barnett', 
+        'ruolo': 'Founder & CEO',
+        'foto': 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome': 'Angela Caroll', 
+        'ruolo': 'Chief Editor',
+        'foto': 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome': 'Walter Gordon', 
+        'ruolo': 'Office Manager',
+        'foto': 'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome': 'Angela Lopez', 
+        'ruolo': 'Social Media Manager',
+        'foto': 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome': 'Scott Estrada', 
+        'ruolo': 'Developer',
+        'foto': 'scott-estrada-developer.jpg'
+    },
+    {
+        'nome': 'Barbara Ramos', 
+        'ruolo': 'Graphic Designer',
+        'foto': 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+// console.log(team)
+
+// stampo per ogni membro del team le informazioni
+for (let i = 0; i < team.length; i++) {
+    console.log(`${i+1}° membro del team:`)
+    for (let key in team[i]) {
+        console.log(team[i][key])
+    }
+}
